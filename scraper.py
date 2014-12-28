1511484adf4b33d3ef8678b0ca3228545c250202
Antoine <antoine@antoine-XPS-L412Z.(none)>
2014-12-28 16:02:16 +0100

    ajout de base + gain de stats

--- scraper.py
+++ scraper.py
@@ -1,10 +1,17 @@
 __author__ = 'antoine'
 
 
-import requests
+import requests, re
 from BeautifulSoup import BeautifulSoup
 
-r = requests.get('http://playdota.com/heroes/admiral')
+#r = requests.get('http://playdota.com/heroes/admiral')
+# we dont spam playdota.com
+r = open("test.html",'r')
+
+
+str = re.compile("^Strength")
+a = re.compile("^Agility")
+i = re.compile("^Intelligence")
 
 hero = {
         'id': 1,
@@ -30,13 +37,46 @@ hero = {
         'introduction': '',
     }
 
+def extract(s):
+    l = []
+    for t in s.split():
+        try:
+            if(t.isdigit()):
+                l.append(t)
+            elif(str.match(t)):
+                hero['baseStrength'] = re.findall("Strength(\d+)", t)[0]
+            elif(a.match(t)):
+                hero['baseAgility'] = re.findall("Agility(\d+)", t)[0]
+            elif(i.match(t)):
+                hero['baseIntelligence'] = re.findall("Intelligence(\d+)", t)[0]
+            else:
+                l.append(float(t))
+        except ValueError:
+            pass
+    return l
 
-if r.status_code == 200:
-    print('Ok:')
-    soup = BeautifulSoup(r.text)
-    name = soup.findAll('h1')
-    name = BeautifulSoup(name).find('img',attrs={'alt':True})
 
-    surname = [h2.text for h2 in soup.findAll('h1', attrs={'class': 'class'})]
-    description = soup.find(id="info").p.text
-    print(name)
+
+
+#if r.status_code == 200:
+print('Ok:')
+soup = BeautifulSoup(r)
+hLeft = soup.find('div',attrs={'class':'hLeft'})
+
+hero['name'] = hLeft.h1.findAll('img',attrs={'alt':True})[0]['alt']
+hero['surname'] = [h2.text for h2 in soup.findAll('h1', attrs={'class': 'class'})][0]
+hero['introduction'] = soup.find(id="info").p.text
+all = [h2.text for h2 in hLeft.findAll('li')]
+
+total = []
+
+for test in all:
+    total.append(extract(test))
+
+hero['gainStrength'] = total[0]
+hero['gainIntelligence'] = total[1][0]
+hero['gainAgility'] = total[2]
+
+
+print hero
+
