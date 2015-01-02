550d297f957b9dcd1e1fdf0f60ce62f828e2155e
hyptos <hyptos974@gmail.com>
2015-01-02 17:32:56 +0100

    must done hRight

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 __author__ = 'antoine'
 
 
-import requests, re
+import requests, re, json
 from BeautifulSoup import BeautifulSoup
 
 #r = requests.get('http://playdota.com/heroes/admiral')
@@ -25,7 +25,8 @@ hero = {
         'gainStrength': '',
         'gainIntelligence': '',
         'affiliation': '',
-        'damage': '',
+        'damageMin': '',
+        'damageMax': '',
         'armor': '',
         'moveSpeed': '',
         'attackRange': '',
@@ -62,21 +63,34 @@ def extract(s):
 print('Ok:')
 soup = BeautifulSoup(r)
 hLeft = soup.find('div',attrs={'class':'hLeft'})
+hRight = soup.find('div',attrs={'class':'hRight'})
 
 hero['name'] = hLeft.h1.findAll('img',attrs={'alt':True})[0]['alt']
 hero['surname'] = [h2.text for h2 in soup.findAll('h1', attrs={'class': 'class'})][0]
 hero['introduction'] = soup.find(id="info").p.text
+
 all = [h2.text for h2 in hLeft.findAll('li')]
 
-total = []
+advanced = hRight.findAll('ul', attrs={'class':'adv'})
 
+for tu in advanced[0]:
+    if tu.li is not None:
+        print tu.li
+
+
+
+#------------------------------
+total = []
 for test in all:
     total.append(extract(test))
 
-hero['gainStrength'] = total[0]
+hero['gainStrength'] = total[0][0]
 hero['gainIntelligence'] = total[1][0]
-hero['gainAgility'] = total[2]
+hero['gainAgility'] = total[2][0]
+
+#------------------------------
 
 
-print hero
 
+
+print json.dumps(hero, indent=4, sort_keys=True)
