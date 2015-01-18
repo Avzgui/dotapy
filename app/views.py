1644431aeac191766555882652904fb807eae92a
Antoine <antoine@antoine-XPS-L412Z.(none)>
2015-01-18 19:33:39 +0100

    manque plus que la main stat

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from app import app
+from app.models import Hero
 from flask import render_template,jsonify
 from app import db, models
 
@@ -11,8 +12,12 @@ def index():
                            Heroes=heroes)
 
 @app.route('/api/v1.0/hero/<int:hero_id>', methods=['GET'])
-def getHero(hero_id):
+def getHeroById(hero_id):
     return jsonify(hero = models.Hero.query.get(hero_id).serialize)
+
+@app.route('/api/v1.0/hero/<hero_name>', methods=['GET'])
+def getHeroByName(hero_name):
+    return jsonify(hero = Hero().query.filter_by(name=hero_name).first().serialize)
 
 @app.errorhandler(404)
 def page_not_found(error):
